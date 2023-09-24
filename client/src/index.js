5e7ede6a0d5e5e8d7c6f64c0f6153ff2fa1a3c32
Aaron <aaronstz@hotmail.com>
2023-09-24 14:06:13 -0300

    hardcodeando deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store';
 import { Provider } from 'react-redux';
-// import axios from "axios"
+import axios from "axios"
 // import dotenv from "dotenv";
 // dotenv.config();
 
